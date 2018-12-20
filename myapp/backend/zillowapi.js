e49b435df3c058f07fd6f161823e69499aa0dac3
Kirby <legendofkirby85@gmail.com>
2018-12-19 20:02:02 -0800

    updated zillowapi with more results

--- myapp/backend/zillowapi.js
+++ myapp/backend/zillowapi.js
@@ -9,8 +9,15 @@ var parameters = {
 }
 zillow.get('GetSearchResults', parameters)
     .then(results => {
-        var zestimate = results.response.results.result[0].zestimate[0].amount;
-        console.log(zestimate)
+        var zestimate = results.response.results.result[0].zestimate[0].amount[0];
+        var homeDetails = results.response.results.result[0].links[0].homedetails[0];
+        var mapThisHome = results.response.results.result[0].links[0].mapthishome[0];
+        var comparables = results.response.results.result[0].links[0].comparables[0];
+
+        console.log(zestimate);
+        console.log(homeDetails);
+        console.log(mapThisHome);
+        console.log(comparables);
         return results
     })
 
