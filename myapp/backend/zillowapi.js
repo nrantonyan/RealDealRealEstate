3a8327f0dafa7f4bffef09e670a2453a661e3a91
Kirby <legendofkirby85@gmail.com>
2018-12-19 19:47:02 -0800

    zillow api working at basic level on terminal

--- myapp/backend/zillowapi.js
+++ myapp/backend/zillowapi.js
@@ -1,15 +1,17 @@
 var Zillow = require("node-zillow")
 
-var zillow = new Zillow(X1-ZWz187ifmasdfv_1fz14)
+var zillow = new Zillow('X1-ZWz187ifmasdfv_1fz14')
 
 var parameters = {
-    address: "2114 Bigelow Ave",
-    citystatezip: "Seattle, WA",
+    address: "12640 Classics Drive",
+    citystatezip: "Granada Hills, CA",
     rentzestimate: false
 }
-
 zillow.get('GetSearchResults', parameters)
-    .then(function(results) {
-        console.log(results)
-        return results;
+    .then(results => {
+        var zestimate = results.response.results.result[0].zestimate[0].amount;
+        console.log(zestimate)
+        return results
     })
+
+
