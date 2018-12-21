2fe0755ded93af17fcd6e33cd83efc1126820c6b
wcollins185 <wcollins185@gmail.com>
2018-12-20 18:50:24 -0800

    Created database and tables. Also installed NPM packets and outlined in the readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RealDealRealEstate
+
+## Installed Libraries (Will)
+
+Please note that we have already created a package.json and saved it to the master branch. Therefore, you will not need to do an npm init. 
+
+The following are libraries that I installed on my branch. 
+  - Express 
+  - Handlebars 
+  - Mysql 
+  - Parser 
+  - Sequelize
---
+++ myapp/db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO user_profile (first_name, last_name, phone_number,
+user_address, username, email, user_password)
+VALUES ("William", "Collins", "6032838424", "1380 Veteran Ave", 
+"wcollins185", "wcollins185@gmail.com", "123456");
+
+INSERT INTO users_projects (project_name, project_address, 
+project_city, project_state, after_repair_value, desired_profit, 
+estimated_repair_cost, buyers_closing_cost, sellers_closing_cost, 
+realtor_fees, monthly_holding_cost, days_to_rehab, project_status)
+VALUES ("house 1", "12345 K st", "Someplace", "California", "780000",
+"60000", "200000", "39000", "20000", "2", "3000", "365","1");
