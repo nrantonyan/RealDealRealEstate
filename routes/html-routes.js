6a7b4011f0eae8b9ddf5214147c41997009496f2
wcollins185 <wcollins185@gmail.com>
2019-01-02 17:52:21 -0800

    WillsServer

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,20 @@
-module.exports = function (app) {
-    
+var path = require("path");
+
+// routes
+module.export = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/results.html"));
+  });
+  app.get("/info", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/info.html"));
+  });
+  app.get("/costs", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/costs.html"));
+  });
+  app.get("/purchase", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/purchase.html"));
+  });
+  app.get("/results", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/results.html"));
+  });
 };
