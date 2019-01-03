5c235af8a74e1333cdd98b5b441387135595cb43
Nick <nickantonyan@gmail.com>
2019-01-02 17:57:33 -0800

    passport start

---
+++ config/passport-google.js
@@ -0,0 +1,8 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy ({
+    clientID: ,
+    clientSecret ,
+    
+}))
