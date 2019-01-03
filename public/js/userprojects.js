0cafa82b50cd86ba91f272a4bd29279c7a359caa
Kirby <legendofkirby85@gmail.com>
2019-01-02 21:19:19 -0800

    updated for project listing per user

--- public/js/userprojects.js
+++ public/js/userprojects.js
@@ -2,39 +2,39 @@ $(document).ready(function() {
     /* global moment */
   
     // blogContainer holds all of our posts
-    var blogContainer = $(".blog-container");
+    var projectContainer = $(".project-container");
     var postCategorySelect = $("#category");
     // Click events for the edit and delete buttons
-    $(document).on("click", "button.delete", handlePostDelete);
+    $(document).on("click", "button.delete", handleProjectDelete);
     $(document).on("click", "button.edit", handlePostEdit);
     // Variable to hold our posts
-    var posts;
+    var projects;
   
     // The code below handles the case where we want to get blog posts for a specific author
     // Looks for a query param in the url for author_id
     var url = window.location.search;
-    var authorId;
-    if (url.indexOf("?author_id=") !== -1) {
-      authorId = url.split("=")[1];
-      getPosts(authorId);
+    var userId;
+    if (url.indexOf("?user_id=") !== -1) {
+      userId = url.split("=")[1];
+      getProjects(userId);
     }
     // If there's no authorId we just get all posts as usual
     else {
-      getPosts();
+      getProjects();
     }
   
   
     // This function grabs posts from the database and updates the view
-    function getPosts(author) {
-      authorId = author || "";
-      if (authorId) {
-        authorId = "/?author_id=" + authorId;
+    function getPosts(user) {
+      userId = user || "";
+      if (userId) {
+        userId = "/?user_id=" + userId;
       }
-      $.get("/api/posts" + authorId, function(data) {
-        console.log("Posts", data);
-        posts = data;
-        if (!posts || !posts.length) {
-          displayEmpty(author);
+      $.get("/api/projects" + userId, function(data) {
+        console.log("Projects", data);
+        projects = data;
+        if (!projects || !projects.length) {
+          displayEmpty(user);
         }
         else {
           initializeRows();
@@ -43,75 +43,75 @@ $(document).ready(function() {
     }
   
     // This function does an API call to delete posts
-    function deletePost(id) {
+    function deleteProject(id) {
       $.ajax({
         method: "DELETE",
-        url: "/api/posts/" + id
+        url: "/api/projects/" + id
       })
         .then(function() {
-          getPosts(postCategorySelect.val());
+          getProjects(postCategorySelect.val());
         });
     }
   
     // InitializeRows handles appending all of our constructed post HTML inside blogContainer
     function initializeRows() {
-      blogContainer.empty();
-      var postsToAdd = [];
-      for (var i = 0; i < posts.length; i++) {
-        postsToAdd.push(createNewRow(posts[i]));
+      projectContainer.empty();
+      var projectsToAdd = [];
+      for (var i = 0; i < projects.length; i++) {
+        projectsToAdd.push(createNewRow(projects[i]));
       }
-      blogContainer.append(postsToAdd);
+      projectsContainer.append(projectsToAdd);
     }
   
     // This function constructs a post's HTML
-    function createNewRow(post) {
-      var formattedDate = new Date(post.createdAt);
+    function createNewRow(project) {
+      var formattedDate = new Date(project.createdAt);
       formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-      var newPostCard = $("<div>");
-      newPostCard.addClass("card");
-      var newPostCardHeading = $("<div>");
-      newPostCardHeading.addClass("card-header");
+      var newProjectCard = $("<div>");
+      newProjectCard.addClass("card");
+      var newProjectCardHeading = $("<div>");
+      newProjectCardHeading.addClass("card-header");
       var deleteBtn = $("<button>");
       deleteBtn.text("x");
       deleteBtn.addClass("delete btn btn-danger");
       var editBtn = $("<button>");
       editBtn.text("EDIT");
       editBtn.addClass("edit btn btn-info");
-      var newPostTitle = $("<h2>");
-      var newPostDate = $("<small>");
-      var newPostAuthor = $("<h5>");
-      newPostAuthor.text("Written by: " + post.Author.name);
-      newPostAuthor.css({
+      var newProjectTitle = $("<h2>");
+      var newProjectDate = $("<small>");
+      var newProjectUser = $("<h5>");
+      newProjectUser.text("Written by: " + project.user.name);
+      newProjectUser.css({
         float: "right",
         color: "blue",
         "margin-top":
         "-10px"
       });
-      var newPostCardBody = $("<div>");
-      newPostCardBody.addClass("card-body");
-      var newPostBody = $("<p>");
-      newPostTitle.text(post.title + " ");
-      newPostBody.text(post.body);
-      newPostDate.text(formattedDate);
-      newPostTitle.append(newPostDate);
-      newPostCardHeading.append(deleteBtn);
-      newPostCardHeading.append(editBtn);
-      newPostCardHeading.append(newPostTitle);
-      newPostCardHeading.append(newPostAuthor);
-      newPostCardBody.append(newPostBody);
-      newPostCard.append(newPostCardHeading);
-      newPostCard.append(newPostCardBody);
-      newPostCard.data("post", post);
-      return newPostCard;
+      var newProjectCardBody = $("<div>");
+      newProjectCardBody.addClass("card-body");
+      var newProjectBody = $("<p>");
+      newProjectTitle.text(project.projectname + " ");
+      newProjectBody.text(project.projectaddress);
+      newProjectDate.text(formattedDate);
+      newProjectTitle.append(newprojectDate);
+      newProjectCardHeading.append(deleteBtn);
+      newProjectCardHeading.append(editBtn);
+      newProjectCardHeading.append(newprojectTitle);
+      newProjectCardHeading.append(newprojectAuthor);
+      newProjectCardBody.append(newprojectBody);
+      newProjectCard.append(newprojectCardHeading);
+      newProjectCard.append(newprojectCardBody);
+      newProjectCard.data("Project", project);
+      return newProjectCard;
     }
   
     // This function figures out which post we want to delete and then calls deletePost
-    function handlePostDelete() {
-      var currentPost = $(this)
+    function handleProjectDelete() {
+      var currentProject = $(this)
         .parent()
         .parent()
         .data("post");
-      deletePost(currentPost.id);
+      deleteProject(currentProject.id);
     }
   
     // This function figures out which post we want to edit and takes it to the appropriate url
@@ -130,12 +130,12 @@ $(document).ready(function() {
       if (id) {
         partial = " for Author #" + id;
       }
-      blogContainer.empty();
+      projectContainer.empty();
       var messageH2 = $("<h2>");
       messageH2.css({ "text-align": "center", "margin-top": "50px" });
       messageH2.html("No posts yet" + partial + ", navigate <a href='/cms" + query +
       "'>here</a> in order to get started.");
-      blogContainer.append(messageH2);
+      projectContainer.append(messageH2);
     }
   
   });
